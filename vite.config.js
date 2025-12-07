4ad2b6ed0a90238a2f2212a05116f81301ec3afc
darrriannna <darynafesyk@gmail.com>
2025-12-07 18:50:22 +0100

    sitemap fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: 'https://rehabscan.se',
-      outDir: 'dist'
-    })
-  ]
+  plugins: [react()]
 })
